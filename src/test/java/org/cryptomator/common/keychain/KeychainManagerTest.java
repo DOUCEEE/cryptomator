a93fc77991faf0445e293d23a721ddf05e45a358
DOUCEEE <119974176+DOUCEEE@users.noreply.github.com>
2024-10-10 14:54:09 -0400

    update tache 2

--- src/test/java/org/cryptomator/common/keychain/KeychainManagerTest.java
+++ src/test/java/org/cryptomator/common/keychain/KeychainManagerTest.java
@@ -54,6 +54,33 @@ public class KeychainManagerTest {
 			Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> latch.await());
 			Assertions.assertTrue(result.get());
 		}
+		/**
+		 Le but de ce test est de vérifier que la méthode changePassphrase fonctionne correctement.
+		 Ça vérifie lorsqu'une passphrase est changé, la nouvelle passphrase doit être correctement stocké et récupérée.
+		 */
+		@Test
+		public void testChangePassphrase() throws KeychainAccessException {
+			// Arrange: Crée une instance de KeychainManager avec une implémentation de MapKeychainAccess pour la gestion des mots de passe
+			KeychainManager keychainManager = new KeychainManager(new SimpleObjectProperty<>(new MapKeychainAccess()));
+
+			// Définit une clé pour stocker la phrase de passe
+			String key = "test-key";
+
+			// Définit l'ancienne phrase de passe qui sera stockée
+			String oldPassphrase = "old-pass";
+
+			// Définit la nouvelle phrase de passe qui sera utilisée pour remplacer l'ancienne
+			String newPassphrase = "new-pass";
+
+			// Act: Stocke l'ancienne phrase de passe sous la clé spécifiée
+			keychainManager.storePassphrase(key, "Test Display", oldPassphrase);
+
+			// Change la phrase de passe pour la clé spécifiée, en remplaçant l'ancienne par la nouvelle
+			keychainManager.changePassphrase(key, "Test Display", newPassphrase);
+
+			// Assert: Vérifie que la nouvelle phrase de passe a été correctement stockée et peut être récupérée
+			Assertions.assertArrayEquals(newPassphrase.toCharArray(), keychainManager.loadPassphrase(key));
+		}
 
 	}
 
